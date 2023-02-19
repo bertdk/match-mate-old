f4c253deaa5f20bbdb7707ccfc2f511e78b3a426
bertdk <bertdk10@gmail.com>
2023-02-19 02:34:11 +0100

    fix: cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TournamentsModule } from './tournaments/tournaments.module';
 
 @Module({
   imports: [MikroOrmModule.forRoot(), TournamentsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
